6d59ce61c98b0ea508c267b4c65262266e5becf0
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-04-03 23:28:43 +0200

    cleaned

--- FinanseBidsnAsks.py
+++ FinanseBidsnAsks.py
@@ -4,25 +4,24 @@ from requests.exceptions import HTTPError
 
 waluty = ('BTC', 'GAME', 'ETH')
 
+interval = 5
 
-def con_test(url):
+def connection_test(url):
     return requests.get(url).ok
 
 
-def asknbid(w):
+def get_offers(w):
     url = f'https://bitbay.net/API/Public/{w}/ticker.json'
-    if not con_test(url):
+    if not connection_test(url):
         return con_test(url)
     response = requests.get(url)
-    a = response.json()['ask']
-    b = response.json()['bid']
-    return a, b
+    return (response.json()['ask'], response.json()['bid'])
 
 
 def off_dis(currency):
     for i in currency:
         try:
-            a, b = asknbid(i)
+            a, b = get_offers(i)
             print('Offers for: ', i, '(prices in dollars)')
             print('ask: ', a)
             print('bid: ', b)
@@ -37,20 +36,25 @@ def off_dis(currency):
 
 def diff_between(currency):
     for i in currency:
-        ask, bid = asknbid(i)
+        ask, bid = get_offers(i)
         spread = round(100 - (1 - (ask - bid) / bid) * 100, 4)
         print("Spread for ", i)
         print(spread, '%')
         sleep(5)
 
 
-off_dis(waluty)
-while 1:
-    try:
-        diff_between(waluty)
-    except HTTPError as http_err:
-        print('ERROR', err)
-        break
-    except Exception as err:
-        print('ERROR', err)
-        break
+def main():
+    off_dis(waluty)
+    while 1:
+        try:
+            diff_between(waluty)
+        except HTTPError as http_err:
+            print('ERROR', err)
+            break
+        except Exception as err:
+            print('ERROR', err)
+            break
+
+
+if __name__ == '__main__':
+    main()
