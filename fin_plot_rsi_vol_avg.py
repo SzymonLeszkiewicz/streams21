1404384e1e950c72c9131b5bac5e88dea0acfb12
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-05-22 21:33:41 +0200

    ch1

--- fin_plot_rsi_vol_avg.py
+++ fin_plot_rsi_vol_avg.py
@@ -16,7 +16,7 @@ def m(a):
     return (sum(a) + 1) / (len(a) + 1)
 
 
-def test_con(url):
+def conect_test(url):
     return requests.get(url).ok
 
 
@@ -35,8 +35,8 @@ def calc_rsi(t, da, ia, bhist):
 
 def vol_calc(cur, t):
     url = f'https://api.bitbay.net/rest/trading/transactions/{cur}'
-    if not test_con(url):
-        return test_con(url)
+    if not conect_test(url):
+        return conect_test(url)
     time = int((datetime.now() - timedelta(0, t)).timestamp()) * 1000  # zmienia format na sekundowy
     par = {"fromTime": time}
     response = requests.request("GET", url, params=par)
@@ -51,8 +51,8 @@ def vol_calc(cur, t):
 
 def asknbid(w):
     url = f'https://api.bitbay.net/rest/trading/ticker/{w}'
-    if not test_con(url):
-        return test_con(url)
+    if not conect_test(url):
+        return conect_test(url)
     response = requests.get(url)
     a = response.json()['ticker']['lowestAsk']
     b = response.json()['ticker']['highestBid']
@@ -66,13 +66,15 @@ for i in range(3):
     axs[i].set_zorder(1)
     axs[i].set_frame_on(False)
 
-w = ('BTC', 'LTC', 'ETH')
+w = ('BTC', 'LSK', 'ETH')
 choice = input("rsi czy vol ?").lower()
 assert choice in ('rsi', 'vol'), 'Wybierz między RSI/VOL'
 avg_section = int(input("Wskaż przedział dla średniej"))
 rsi_section = int(input("Wskaż przedział dla RSI"))
 czas = []
 avg = [[], [], []]
+avga = [[], [], []]
+avgb = [[], [], []]
 vol = [[], [], []]
 rsi = [[], [], []]
 
@@ -87,6 +89,7 @@ def animate(i):
         oferts[i + 'asks'] = oferts[i + 'asks'][-5:]
         oferts[i + 'bids'] = oferts[i + 'bids'][-5:]
     oferts_list = list(oferts.values())
+    print(oferts_list)
     now = datetime.now()
     czas.append(now.strftime("%H:%M:%S"))
     for i, ax in enumerate(axs):
@@ -97,20 +100,26 @@ def animate(i):
         vol[i].append(vol_calc(f'{w[i]}-PLN', 100))
         avg[i].append(mean(oferts_list[2 * i][-avg_section:] + oferts_list[2 * i + 1][-avg_section:]))
 
+        avga[i].append(mean(oferts_list[2 * i][-avg_section:]))
+        avgb[i].append(mean(oferts_list[2 * i + 1][-avg_section:]))
+
         rsi[i].append(calc_rsi(rsi_section, dec[i], inc[i], oferts_list[2 * i + 1]))
         axs[1].set_ylabel("value")
+        axs[i].plot(czas[-5:], avga[i][-5:], label='avg asks')
+        axs[i].plot(czas[-5:], avgb[i][-5:], label='avg bids')
         axs[i].plot(czas[-5:], oferts_list[2 * i], label=w[i] + ' asks')
         axs[i].plot(czas[-5:], oferts_list[2 * i + 1], label=w[i] + ' bids')
-        axs[i].plot(czas[-5:], avg[i][-5:], label='avg')
+        # axs[i].plot(czas[-5:], avg[i][-5:], label='avg')
+
         axs[i].legend(loc='upper left')
         # axs[i].set_title(w[i])
         if choice == 'vol':
             pointers[i].bar(czas[-5:], vol[i][-5:], width=0.2, label="vol", color='silver')
-            pointers[i].legend(loc='lower left')
+            pointers[i].legend(loc='center right')
             pointers[1].set_ylabel("volume value")
         else:
             pointers[i].plot(czas[-5:], rsi[i][-5:], color='red', label=f'{w[i]} RSI')
-            pointers[i].legend(loc='lower left')
+            pointers[i].legend(loc='center right')
             pointers[1].set_ylabel("RSI value")
 
 
